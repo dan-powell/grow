951760c4b8d2d682f265e00919502ec9c10da779
Dan Powell <dan@autumnrain.co.uk>
2026-02-08 15:42:32 +0000

    Updates for PHP 8.4

---
+++ rector.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/app',
+        __DIR__ . '/config',
+        __DIR__ . '/database',
+        __DIR__ . '/routes',
+        __DIR__ . '/tests',
+    ])
+    ->withPhpVersion(80400) // PHP 8.4
+    ->withSets([
+        LevelSetList::UP_TO_PHP_84,
+        SetList::CODE_QUALITY,
+        SetList::DEAD_CODE,
+        SetList::EARLY_RETURN,
+        // SetList::INSTANTIATION_TO_STATIC_CALL,
+        SetList::PRIVATIZATION,
+        SetList::STRICT_BOOLEANS,
+    ])
+    ->withSkip([
+        // Skip rules that might cause issues
+        \Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector::class,
+        \Rector\Php80\Rector\FunctionLike\MixedTypeRector::class,
+    ]);
+    // ->withImportShortClasses(false);
